6bbb3efcba40c75776109ab443a02f60dc80119f
jaz801 <34124216+jaz801@users.noreply.github.com>
2023-04-26 13:32:50 +0200

    Update instructions.js

--- instructions.js
+++ instructions.js
@@ -1,15 +1,24 @@
 
 Use the javascript console in chrome.
 
-scroll down so you get all the vacancies. 
-
-
 const elements = document.querySelectorAll("div.BjJfJf.PUpOsf, div.vNEEBe, div.Qk80Jf");
 
+let csvContent = "data:text/csv;charset=utf-8,function,company,foundBy\n";
+
 elements.forEach((element) => {
-  if (element.classList.contains("Qk80Jf") && element.textContent.includes("via")) {
-    console.log(element);
-  } else if (!element.classList.contains("Qk80Jf")) {
-    console.log("Other element:", element);
+  if (element.classList.contains("BjJfJf") && element.textContent.trim() !== "") {
+    csvContent += `"${element.textContent.trim()}",,`;
+  } else if (element.classList.contains("vNEEBe") && element.textContent.trim() !== "") {
+    csvContent += `,"${element.textContent.trim()}",`;
+  } else if (element.classList.contains("Qk80Jf") && element.textContent.includes("via")) {
+    csvContent += `,,${element.textContent.trim()}\n`;
   }
 });
+
+const encodedUri = encodeURI(csvContent);
+const link = document.createElement("a");
+link.setAttribute("href", encodedUri);
+link.setAttribute("download", "output.csv");
+document.body.appendChild(link); 
+link.click();
+
